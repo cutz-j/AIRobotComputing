b5b605009db9da8b0c2d800f8be3b8d805d17268
Hyeonseong Jeon <cutz309@gmail.com>
2019-04-10 14:02:41 +0900

    Update quiz2.py

--- quiz2.py
+++ quiz2.py
@@ -4,12 +4,15 @@ import os
 os.chdir("d:/lecture/AIrobotComputing")
 
 all_data = pd.read_csv("BEST MOVIES of 2017.txt", sep="\t", index_col=0, header=None)
-arr_idx = []
-for i in range(len(all_data.iloc[:, 6])):
-    if 'Comedy' in all_data.iloc[i, 6]:
-        arr_idx.append(i)
 
-comedy = all_data.iloc[arr_idx]
+Comedy_bool = all_data[7].str.contains("Comedy")
+
+comedy = all_data[Comedy_bool]
+
+#arr_idx = []
+#for i in range(len(all_data.iloc[:, 6])):
+#    if 'Comedy' in all_data.iloc[i, 6]:
+#        arr_idx.append(i)
 
 comedy_sort = comedy.sort_values(by=4, ascending=False)
 
